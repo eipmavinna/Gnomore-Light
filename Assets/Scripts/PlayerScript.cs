a0219eaf5c2e84a39609f109bbe029c9c50cf73a
HAGENSV22 <HagenSV22@gcc.edu>
2025-10-29 13:05:33 -0400

    Update player script to detect collecting bugs

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -9,17 +9,27 @@ public class PlayerScript : MonoBehaviour
     float horizontalDirection = 0;
 
     Rigidbody2D _rbody;
+    HudManagerScript _hudManager;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _rbody = GetComponent<Rigidbody2D>();
+        _hudManager = FindAnyObjectByType<HudManagerScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bug"))
+        {
+            _hudManager.OnBugFound(collision.gameObject);
+        }
     }
 
     private void FixedUpdate()
