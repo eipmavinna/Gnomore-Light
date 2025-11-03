a8e5d9cfeef44b17c4c29f490a428860b38877f7
HAGENSV22 <HagenSV22@gcc.edu>
2025-11-03 16:32:28 -0500

    Add timer to hud

--- Assets/Scripts/HudManagerScript.cs
+++ Assets/Scripts/HudManagerScript.cs
@@ -3,23 +3,36 @@ using UnityEngine;
 
 public class HudManagerScript : MonoBehaviour
 {
-
     public TMP_Text bugTracker;
+    public GameObject timeTracker;
+    public float timeLimit;
 
     int bugsCollected;
     int totalBugs;
+    float timeRemaining;
+    float timeBarWidth;
+    float lastUpdateTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        lastUpdateTime = Time.time;
+        timeBarWidth = timeTracker.transform.localScale.x;
+
+        if (PlayerPrefs.GetInt("InLevel", 0) == 0)
+        {
+            PlayerPrefs.SetFloat("TimeRemaining", timeLimit);
+        }
+        PlayerPrefs.SetInt("InLevel", 1);
         bugsCollected = PlayerPrefs.GetInt("BugsCollected", 0);
         totalBugs = PlayerPrefs.GetInt("TotalBugs", 0);
+        timeRemaining = PlayerPrefs.GetFloat("TimeRemaining", timeLimit);
         UpdateBugTracker();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateTimeRemaining();
     }
 
     public void OnBugFound(GameObject bug)
@@ -28,6 +41,19 @@ public class HudManagerScript : MonoBehaviour
         bugsCollected += 1;
         PlayerPrefs.SetInt("BugsCollected", bugsCollected);
         UpdateBugTracker();
+    }
+
+    void UpdateTimeRemaining()
+    {
+        float deltaTime = Time.time - lastUpdateTime;
+        timeRemaining -= deltaTime;
+        if (timeRemaining <= 0)
+        {
+            timeRemaining = 0;
+        }
+        lastUpdateTime = Time.time;
+        PlayerPrefs.SetFloat("TimeRemaining", timeRemaining);
+        timeTracker.transform.localScale = new Vector3((timeRemaining / timeLimit) * timeBarWidth, 1, 1);
     }
 
     void UpdateBugTracker()
