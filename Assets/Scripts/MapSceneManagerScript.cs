29e8e9534789c36c025a0d5d0b01297b418b41aa
eipmavinna <146777395+eipmavinna@users.noreply.github.com>
2025-12-05 19:27:01 -0500

    fixed some bugs with the tutorial scene

--- Assets/Scripts/MapSceneManagerScript.cs
+++ Assets/Scripts/MapSceneManagerScript.cs
@@ -24,20 +24,20 @@ public class MapSceneManagerScript : MonoBehaviour
         
     }
 
-    public void LoadWoodsLevel()
-    {
-        int total = PlayerPrefs.GetInt("TotalBugs");
-        PlayerPrefs.SetInt("TotalBugs", 10);
-        Debug.Log( "total before: " + total.ToString() + " after: "  + PlayerPrefs.GetInt("TotalBugs").ToString());
+    //public void LoadWoodsLevel()
+    //{
+    //    int total = PlayerPrefs.GetInt("TotalBugs");
+    //    PlayerPrefs.SetInt("TotalBugs", 10);
+    //    Debug.Log( "total before: " + total.ToString() + " after: "  + PlayerPrefs.GetInt("TotalBugs").ToString());
 
-        UnityEngine.SceneManagement.SceneManager.LoadScene("TheWoodsScene");
-    }
+    //    UnityEngine.SceneManagement.SceneManager.LoadScene("TheWoodsScene");
+    //}
 
-    public void LoadGrasslandsLevel()
-    {
-        int total = PlayerPrefs.GetInt("TotalBugs");
-        PlayerPrefs.SetInt("TotalBugs", 10);
-        Debug.Log("total before: " + total.ToString() + " after: " + PlayerPrefs.GetInt("TotalBugs").ToString());
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Grasslands");
-    }
+//    public void LoadGrasslandsLevel()
+//    {
+//        int total = PlayerPrefs.GetInt("TotalBugs");
+//        PlayerPrefs.SetInt("TotalBugs", 10);
+//        Debug.Log("total before: " + total.ToString() + " after: " + PlayerPrefs.GetInt("TotalBugs").ToString());
+//        UnityEngine.SceneManagement.SceneManager.LoadScene("Grasslands");
+//    }
 }
