fa7f875b8205e792181e15a68c532c3d8643ce04
HAGENSV22 <HagenSV22@gcc.edu>
2025-12-08 15:00:08 -0500

    Map scene changes

--- Assets/Scripts/MapSceneManagerScript.cs
+++ Assets/Scripts/MapSceneManagerScript.cs
@@ -1,12 +1,30 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
+using Unity.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 public class MapSceneManagerScript : MonoBehaviour
 {
-    public TMP_Text bugCounter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TMP_Text headerText;
+    public TMP_Text instructionsText;
+    public TMP_Text statsHeader;
+    public TMP_Text bugsRequired;
+    public TMP_Text mostBugsCollected;
+    public TMP_Text bestTime;
+    public GameObject playButton;
+
+
+    LevelData selectedLevel;
     void Start()
     {
-        bugCounter.text = "Bugs collected: " + PlayerPrefs.GetInt("MapTotalBugs").ToString();
+        //Add levels up here (id [Scene Name], name, bugs required)
+        LevelData.addLevel("TheWoodsScene","The Woods",10);
+
+
+        showInstructions(true);
+
         string bugsLoaded = PlayerPrefs.GetString("BugsLoaded", "");
         string[] bugs = bugsLoaded.Split(';');
         foreach (var item in bugs)
@@ -21,23 +39,116 @@ public class MapSceneManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    //public void LoadWoodsLevel()
-    //{
-    //    int total = PlayerPrefs.GetInt("TotalBugs");
-    //    PlayerPrefs.SetInt("TotalBugs", 10);
-    //    Debug.Log( "total before: " + total.ToString() + " after: "  + PlayerPrefs.GetInt("TotalBugs").ToString());
+    void showInstructions(bool show)
+    {
+        headerText.text = "Controls";
+        instructionsText.enabled = show;
+        bugsRequired.enabled = !show;
+        statsHeader.enabled = !show;
+        mostBugsCollected.enabled = !show;
+        bestTime.enabled = !show;
+        playButton.SetActive(!show);
+    } 
+        
 
-    //    UnityEngine.SceneManagement.SceneManager.LoadScene("TheWoodsScene");
-    //}
+    public void LoadLevelInfo(string level)
+    {
+        selectedLevel = LevelData.getLevel(level);
+        if (selectedLevel == null)
+        {
+            headerText.text = "Controls";
+            showInstructions(true);
+            return;
+        }
 
-//    public void LoadGrasslandsLevel()
-//    {
-//        int total = PlayerPrefs.GetInt("TotalBugs");
-//        PlayerPrefs.SetInt("TotalBugs", 10);
-//        Debug.Log("total before: " + total.ToString() + " after: " + PlayerPrefs.GetInt("TotalBugs").ToString());
-//        UnityEngine.SceneManagement.SceneManager.LoadScene("Grasslands");
-//    }
+        showInstructions(false);
+        headerText.text = selectedLevel.GetName();
+        bugsRequired.text = "Bugs Required: " + selectedLevel.GetBugsRequired();
+
+        mostBugsCollected.text = "Highest Collected: " + selectedLevel.GetMostBugs();
+        bestTime.text = "Best Time: " + selectedLevel.GetBestTime();
+    }
+
+    public void PlaySelectedLevel()
+    {
+        if (selectedLevel == null) {
+            return;
+        }
+
+        selectedLevel.LoadLevel();
+    }
+
+
+    public class LevelData
+    {
+        private static Dictionary<string, LevelData> levels = new Dictionary<string, LevelData>();
+
+        private int bugsRequired;
+        private string levelId;
+        private string levelName;
+
+        public static void addLevel(string levelId, string name, int bugsRequired)
+        {
+            LevelData data = new LevelData(levelId, name, bugsRequired);
+            levels[levelId] = data;
+        }
+
+        public static LevelData getLevel(string levelId)
+        {
+            return levels[levelId];
+        }
+
+        private LevelData(string levelId, string name, int bugsRequired)
+        {
+            this.levelId = levelId;
+            this.bugsRequired = bugsRequired;
+            this.levelName = name;
+        }
+
+        public string GetLevelId()
+        {
+            return levelId;
+        }
+
+        public int GetBugsRequired()
+        {
+            return bugsRequired;
+        }
+
+        public string GetName()
+        {
+            return levelName;
+        }
+
+        public int GetMostBugs()
+        {
+            return PlayerPrefs.GetInt(levelId + "BugsBest", 0);
+        }
+
+        public string GetBestTime()
+        {
+            return FormatTime(PlayerPrefs.GetFloat(levelId + "TimeBest", 0));
+        }
+
+        public void LoadLevel()
+        {
+            PlayerPrefs.SetInt("TotalBugs", bugsRequired);
+            PlayerPrefs.SetString("CurrentLevel", levelId);
+            SceneManager.LoadScene(levelId);
+        }
+
+        string FormatTime(float time)
+        {
+            float minutes = Mathf.Floor(time / 60);
+            float seconds = time % 60;
+            if (seconds < 10)
+            {
+                return minutes + $":0{seconds:F2}";
+            }
+            return minutes + $":{seconds:F2}";
+        }
+    }
 }
